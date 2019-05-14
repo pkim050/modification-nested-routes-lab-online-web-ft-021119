822a8a83216588910481957ce231c1c3765a9dae
Patrick Kim <pkim050@gmail.com>
2019-05-14 18:18:05 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -7,7 +7,7 @@ module ArtistsHelper
     if song.artist.nil?
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name) #collection_select(:song, :artist_id, Artist.all, :id, :name)
     else
-      #hidden_field_tag "song[artist_id]", song.artist_id #hidden_field(:song, :artist_id)
+      hidden_field(:song, :artist_id)
     end
   end
 end
